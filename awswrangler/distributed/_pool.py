7f70b65c373c5f7b46d5a8884faa37b38dbda188
Anton Kukushkin <3997468+kukushking@users.noreply.github.com>
2022-07-14 16:00:21 +0100

    change from apply_async to starmap

--- awswrangler/distributed/_pool.py
+++ awswrangler/distributed/_pool.py
@@ -11,14 +11,24 @@ _logger: logging.Logger = logging.getLogger(__name__)
 
 
 class _RayPoolExecutor:
-    def __init__(self, processes: Optional[Union[bool, int]] = None):
-        self._exec: Pool = Pool(processes=None if isinstance(processes, bool) else processes)
+    def __init__(
+        self,
+        processes: Optional[Union[bool, int]] = None,
+        maxtasksperchild: Optional[int] = None,
+        chunksize: Optional[int] = None
+    ):
+        self._exec: Pool = Pool(
+            processes=None if isinstance(processes, bool) else processes,
+            maxtasksperchild=maxtasksperchild,
+        )
+        # Static for now - pass to map later configuring depending on the task
+        self._chunksize: Optional[int] = chunksize
 
     def map(self, func: Callable[..., List[str]], _: boto3.Session, *args: Any) -> List[Any]:
         """Map function and its args to Ray pool."""
-        futures = []
         _logger.debug("Ray map: %s", func)
-        # Discard boto3.Session object & call the fn asynchronously
-        for arg in zip(itertools.repeat(None), *args):
-            futures.append(self._exec.apply_async(func, arg))
-        return [f.get() for f in futures]
+        # Discard boto3.Session object, call map unpacking the arguments ("starmap"), and block
+        # until the results are available
+        return self._exec.starmap(
+            func, zip(itertools.repeat(None), *args), chunksize=self._chunksize,
+        )
